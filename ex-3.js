db113ef6d7c860badfb41af57795591a4d1ab545
Ja <ja.techup@gmail.com>
2024-05-15 14:14:01 +0700

    feat:js ex1-7

--- ex-3.js
+++ ex-3.js
@@ -3,3 +3,9 @@
 let lightBulbStatus = "Off";
 
 //Start coding here
+
+lightBulbStatus === "On"
+  ? console.log("Light bulb is On.")
+  : lightBulbStatus === "Off"
+  ? console.log("Light bulb is Off.")
+  : console.log("Please choose the correct input (On/Off)");
